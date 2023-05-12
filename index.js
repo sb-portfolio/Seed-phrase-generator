2a79bbbacc15bc8a57f9005535ab1a450122cc42
sb-portfolio <sibond3@gmail.com>
2023-05-12 10:26:12 +0100

    Update html

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import wordsArray from "./words.js"
 
 const seedWords = Object.values(wordsArray);
-const seedPhraseLength = 12;
-const numberOfSeedWords = seedWords.length
-let randomSeedPosition
-let chosenWord = ""
-let seedPhrase = [];
+const seedPhraseLengthEl = document.getElementById("seed-length")
+const generateSeedPhraseBtn = document.getElementById("generate-btn")
+const seedPhraseOutputEl = document.getElementById("seed-phrase-output")
+generateSeedPhraseBtn.addEventListener("click", createSeedPhrase)
 
-while (seedPhrase.length<seedPhraseLength){
+
+
+function createSeedPhrase(){
+    const seedPhraseLength = seedPhraseLengthEl.value;
+    const numberOfSeedWords = seedWords.length
+    let randomSeedPosition
+    let chosenWord = ""
+    let seedPhrase = [];
     
-    randomSeedPosition = Math.floor(Math.random()*numberOfSeedWords);
-    chosenWord = seedWords[randomSeedPosition];
-
-    if(!seedPhrase.includes(chosenWord)){
-        seedPhrase.push(chosenWord);
+    while (seedPhrase.length<seedPhraseLength){
+    
+        randomSeedPosition = Math.floor(Math.random()*numberOfSeedWords);
+        chosenWord = seedWords[randomSeedPosition];
+    
+        if(!seedPhrase.includes(chosenWord)){
+            seedPhrase.push(chosenWord);
+        }
     }
+    seedPhraseOutputEl.innerHTML = ""
+    seedPhrase.forEach(word => {
+        seedPhraseOutputEl.innerHTML += `<div class="seed-word">${word[0].toUpperCase()}${word.slice(1)}</div>`
+    })
+    console.log(seedPhrase)
 }
-
-
-console.log(seedPhrase)
