0e2f5afceaf205df5cbbb077c36288512f488861
sb-portfolio <sibond3@gmail.com>
2023-05-12 12:46:50 +0100

    Clean code

--- index.js
+++ index.js
@@ -3,26 +3,34 @@ import {wordsArray as seedWords} from "./words.js"
 const seedPhraseLengthEl = document.getElementById("seed-length")
 const generateSeedPhraseBtn = document.getElementById("generate-btn")
 const seedPhraseOutputEl = document.getElementById("seed-phrase-output")
+const numberOfSeedWords = seedWords.length
+
 generateSeedPhraseBtn.addEventListener("click", createSeedPhrase)
+
+const randomSeedPosition = () => Math.floor(Math.random()*numberOfSeedWords);
+const randomWord = () => seedWords[randomSeedPosition()];
+const wordNotAlreadyUsed = (array, word) => !array.includes(word)
+
+function renderSeedPhrase(seedPhraseArray){
+    seedPhraseOutputEl.innerHTML = ""
+
+    seedPhraseArray.forEach(word => {
+        seedPhraseOutputEl.innerHTML += `<div class="seed-word">${word[0].toUpperCase()}${word.slice(1)}</div>`
+    })
+}
 
 function createSeedPhrase(){
     const seedPhraseLength = seedPhraseLengthEl.value;
-    const numberOfSeedWords = seedWords.length
-    let randomSeedPosition
     let chosenWord = ""
     let seedPhrase = [];
     
     while (seedPhrase.length<seedPhraseLength){
-    
-        randomSeedPosition = Math.floor(Math.random()*numberOfSeedWords);
-        chosenWord = seedWords[randomSeedPosition];
-    
-        if(!seedPhrase.includes(chosenWord)){
+
+        chosenWord = randomWord()
+
+        if(wordNotAlreadyUsed(seedPhrase, chosenWord)){
             seedPhrase.push(chosenWord);
         }
     }
-    seedPhraseOutputEl.innerHTML = ""
-    seedPhrase.forEach(word => {
-        seedPhraseOutputEl.innerHTML += `<div class="seed-word">${word[0].toUpperCase()}${word.slice(1)}</div>`
-    })
+    renderSeedPhrase(seedPhrase)
 }
